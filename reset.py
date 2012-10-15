cc3ed9de1cc2a7776e0578c6bb1b90b65ba91fb6
J Phani Mahesh <phanimahesh@gmail.com>
2012-10-16 02:20:17 +0530

    fixed reset.py once again

--- reset.py
+++ reset.py
@@ -19,7 +19,7 @@ def resetUnityChildren():
     unityChildren=["com.canonical.Unity","com.canonical.Unity.Launcher","com.canonical.Unity.ApplicationsLens","com.canonical.Unity.Lenses","com.canonical.Unity.Dash","com.canonical.Unity.Panel","com.canonical.Unity.Devices","com.canonical.Unity.Runner","com.canonical.Unity.FilesLens"]
     for child in unityChildren:
 #    subprocess.Popen(unityresetter,shell=True, stdout=subprocess.PIPE).communicate()
-         subprocess.call("gsettings reset-recursively "+child)
+         subprocess.call("gsettings reset-recursively "+child,shell=True)
 # Replace the current session, reloading settings.
 resetPlugins()
 resetUnityChildren()
