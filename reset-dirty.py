59aeff407ac857ae8cb8cf27bace8a0393d9ee57
J Phani Mahesh <phanimahesh@gmail.com>
2012-10-15 11:06:04 +0530

    Separated dirty and better ones

--- reset-dirty.py
+++ reset-dirty.py
@@ -1,15 +1,17 @@
 # Unity reset script for Quantal.
 # born at http://chat.stackexchange.com/rooms/6118/unity-reconfiguration
-
-#Better version
+# Dirty one
 
 #!/usr/bin/python
 import subprocess
 plugins=['core', 'composite', 'opengl', 'decor', 'vpswitch', 'snap', 'mousepoll', 'resize', 'place', 'move', 'wall', 'grid', 'session', 'animation', 'fade', 'unitymtgrabhandles', 'workarounds', 'scale', 'expo', 'ezoom', 'unityshell']
 for plugin in plugins:
     schema='org.compiz.'+plugin
+    n="gsettings list-keys "+schema+":/org/compiz/profiles/unity/"
+    out,err=subprocess.Popen(n,shell=True, stdout=subprocess.PIPE).communicate()
+    keys=out.splitlines()
     path="/org/compiz/profiles/unity/plugins/"+plugin+"/"
-    resetter="gsettings reset-recursively "+schema+":"+path
-    subprocess.call(resetter,shell=True)
+    for key in keys:
+        resetter="gsettings reset "+schema+":"+path+" "+key
+        subprocess.call(resetter,shell=True)
 subprocess.call("setsid unity",shell=True)
-
