e3e1588194c5789d1b5ad0d3cb1ce81805792864
J Phani Mahesh <phanimahesh@gmail.com>
2012-10-18 20:16:49 +0530

    fixed namespace error

--- unityunittests.py
+++ unityunittests.py
@@ -9,13 +9,13 @@ class TestUnityReset(unittest.TestCase):
 '''Unittest for unity --reset'''
     def setUp(self):
         # Get defaults
-        compizPlugins=unityreset.UnityReset.snapshotCompizPlugins()
-        unityChildren=unityreset.UnityReset.snapshotUnityChildren()
+        self.ompizPlugins=unityreset.UnityReset.snapshotCompizPlugins()
+        self.unityChildren=unityreset.UnityReset.snapshotUnityChildren()
 
     def test_reset_display_recent_apps(self):
         schema="com.canonical.Unity.ApplicationsLens"
         key="display-recent-apps"
-        default=unityChildren[schema][key]
+        default=self.unityChildren[schema][key]
         gsettings=Gio.Settings(schema)
         gsettings.set_boolean(key,False)
         unityreset.UnityReset(False)
